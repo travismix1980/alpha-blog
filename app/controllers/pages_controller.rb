1abce76755ade693e11645d84b9372ffa3d96b38
Travis Mix <travismix1980@gmail.com>
2020-10-24 18:10:46 -0700

    Assignment complete but its kinda ugly

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Pages Controller / default controller for our app
+class PagesController < ApplicationController
+
+  def home; end
+
+  def about; end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home' # default root route of our application
+  get 'about', to: 'pages#about' # our about page route
 end
